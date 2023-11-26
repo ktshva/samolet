827b6ce5d02022f7b8af358ee95eb242698ea6a4
Arina Kotysheva <kotysheva.arina@gmail.com>
2023-11-26 18:22:22 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 14236; // стоимость билета
+        int miles = 20; // количество рублей для одной бонусной мили
+
+        Object bonus = ticket / miles;
+        System.out.println(bonus);
+    }
+}
